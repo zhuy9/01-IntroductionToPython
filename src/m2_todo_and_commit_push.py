62562d376e93b78d795e453d2876c987d4c670c3
zhuy9 <zhuy9@rose-hulman.edu>
2018-08-30 12:19:04 -0400

    commit

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -85,7 +85,7 @@ print(3607 * 34227)
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).  Note that
 #   this file (m2_todo_and_commit_push.py) is now displayed in BLUE.
 #
